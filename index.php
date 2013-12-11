68cb77e4e71bbe7ca9decb8de26738abe4882637
Christer Jonassen <christer@csdnserver.com>
2013-12-11 19:09:39 +0100

    HTML validation test

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 
 <head>
 	<meta charset="UTF-8" />
-	<title>tetag...k</title>
+	<title>tetag...</title>
 	<link rel="stylesheet" type="text/css" href="tatag.css">
 	<link rel="icon" type="image/png" href="favicon.png">
 </head>
 
 <body>
-	
+
 	<div id="innhold">
 		<form action="index.php" method="post">
 			Adresse:<input id="adressefelt" type="text" name="adresse">
@@ -17,7 +17,7 @@
 		<?php
 		if (isset($_POST['submitknapp']))
 		{
-			$adresse=$_POST['adresse']; 
+			$adresseclean=htmlentities($_POST['adresse'], ENT_QUOTES) 
 			?>
 			<div id="url">
 				<?php
