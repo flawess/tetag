ad8eb60bcb597d1f3c7f11b1f8c9a1692a79dbe6
Christer Jonassen <chr1573r@users.noreply.github.com>
2016-10-27 23:17:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,7 @@
 <body>
 
 	<div id="innhold">
-		<form action="index.php" method="post">
-			Adresse:<input id="adressefelt" type="text" name="adresse">
-			<input id="submitknapp" type="submit" name="submitknapp" value="submitknapp">
-		</form>
+
 		<?php
 		if (isset($_POST['submitknapp']))
 		{
